0d9d3caca72e30143af1c3a5abf7ae1fed49efb2
covidsanki <106461308+covidsanki@users.noreply.github.com>
2022-05-30 08:22:55 +0530

    Update alive.py

--- FallenRobot/modules/alive.py
+++ FallenRobot/modules/alive.py
@@ -11,21 +11,17 @@ from FallenRobot import telethn as tbot
 
 
 PHOTO = [
-    "https://te.legra.ph/file/5481b59e4a31fca4499fe.jpg",
-    "https://te.legra.ph/file/85a67867942ff92c5622b.jpg",
-    "https://te.legra.ph/file/7a81bab81fb73527b2ad3.jpg",
-    "https://te.legra.ph/file/66091a9f4514e8b626ac5.jpg",
-    "https://te.legra.ph/file/be5d28bb10a47ce8e6060.jpg",
+    "https://te.legra.ph/file/64cf39a04497d9deade8b.jpg",
 ]
 
 @register(pattern=("/alive"))
 async def awake(event):
   TEXT = f"**ʜᴇʏ​ [{event.sender.first_name}](tg://user?id={event.sender.id}),\n\nɪ ᴀᴍ ᴀɢᴏʀᴀ ʙᴏᴛ​~🕉️**\n━━━━━━━━━━━━━━━━━━━\n\n"
-  TEXT += f"» **ᴍʏ ᴅᴇᴠᴇʟᴏᴘᴇʀ​ : [Λ𝖦ՕᏒΛ](https://t.me/mr_agora)** \n\n"
+  TEXT += f"» **ᴍʏ ᴅᴇᴠᴇʟᴏᴘᴇʀ​ : [ARYA](https://t.me/KASHYAP_IS_PRO)** \n\n"
   TEXT += f"» **ʟɪʙʀᴀʀʏ ᴠᴇʀsɪᴏɴ :** `{telever}` \n\n"
   TEXT += f"» **ᴛᴇʟᴇᴛʜᴏɴ ᴠᴇʀsɪᴏɴ :** `{tlhver}` \n\n"
   TEXT += f"» **ᴘʏʀᴏɢʀᴀᴍ ᴠᴇʀsɪᴏɴ :** `{pyrover}` \n━━━━━━━━━━━━━━━━━\n\n"
-  BUTTON = [[Button.url("ʜᴇʟᴘ​", "https://t.me/agora_robot?start=help"), Button.url("sᴜᴘᴘᴏʀᴛ​", "https://t.me/karunada_kings_and_queens")]]
+  BUTTON = [[Button.url("ʜᴇʟᴘ​", "https://t.me/MissAyra_Robot?start=help"), Button.url("sᴜᴘᴘᴏʀᴛ​", "https://t.me/HdNetworkOp")]]
   ran = random.choice(PHOTO)
   await tbot.send_file(event.chat_id, ran, caption=TEXT,  buttons=BUTTON)
 
